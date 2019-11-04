83f159409fa3a3ea7b1d51baa56ad82617ccc918
Egbert <m@egbert.co>
2019-11-04 22:29:06 +0100

    initial commit

---
+++ admin/login.php
@@ -0,0 +1,13 @@
+<form id="form1" name="form1" method="post" action="login.php">
+  <p>
+    <label>
+      <input type="text" name="name" id="name" />
+    </label>
+  </p>
+  <p>
+    <input type="password" name="password" id="password" />
+    <label>
+      <input type="submit" name="Login" id="Login" value="Login" />
+    </label>
+  </p>
+</form>
